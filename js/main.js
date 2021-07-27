8e42d6bda60e62b3d567ef12fce7fe6a9bb6a749
sh0402 <dgryoma0402@gmail.com>
2021-07-27 23:18:06 +0900

    0727_commit_home

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ $(function () {
         $("body").toggleClass("nav-open");
     });
 });
+
+
+$("#cardCarousel").click(function () {
+    if ($("span", this).hasClass("icon-Pause")) {
+        $('#carouselExampleControls').carousel('pause');
+        $(this).attr("aria-label", "Play Carousel");
+        $(this).attr("title", "Play Carousel");
+        $("span", this).toggleClass("icon-Pause icon-Play");
+    } else {
+        $('#carouselExampleControls').carousel('cycle');
+        $(this).attr("aria-label", "Pause Carousel");
+        $(this).attr("title", "Pause Carousel");
+        $("span", this).toggleClass("icon-Play icon-Pause");
+    }
+});
