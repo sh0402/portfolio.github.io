348db8d515742d9f094a94c81d2646d35ba878cc
sh0402 <dgryoma0402@gmail.com>
2021-07-30 18:20:33 +0900

    0730_commit_corp

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-$(function () {
-    $(".nav-button").click(function () {
-        $("body").toggleClass("nav-open");
-    });
+
+const toggleBtn = document.querySelector('.nav-button');
+
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
 });
 
-controlNextBtn = function() {
-    var _scrollX = $('.cardCarousel').scrollLeft();
-    $('.cardCarousel').scrollLeft(_scrollX + 150);
-};
 
-controlPrevBtn = function() {
-    var _scrollX = $('.cardCarousel').scrollLeft();
-    $('.cardCarousel').scrollLeft(_scrollX - 150);
-};
+const cardCarouselNext = document.querySelector('.cardCarousel-control-next');
+const cardCarouselPrev = document.querySelector('.cardCarousel-control-prev');
+const cardCarousel = document.querySelector('.cardCarousel');
+
+cardCarouselPrev.addEventListener('click', () => {
+    cardCarousel.scrollLeft -= 150
+});
+
+cardCarouselNext.addEventListener('click', () => {
+    cardCarousel.scrollLeft += 150
+});
+
+
+// controlNextBtn = function() {
+//     var _scrollX = $('.cardCarousel').scrollLeft();
+//     $('.cardCarousel').scrollLeft(_scrollX + 150);
+// };
+
+// controlPrevBtn = function() {
+//     var _scrollX = $('.cardCarousel').scrollLeft();
+//     $('.cardCarousel').scrollLeft(_scrollX - 150);
+// };
+
