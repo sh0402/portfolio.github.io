0ba745a886fbfecd01a38ab596ca50318ab33a84
sh0402 <dgryoma0402@gmail.com>
2021-08-18 18:14:15 +0900

    0818_commit_corp

---
+++ js/slide.js
@@ -0,0 +1,59 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+// $(document).ready(function () {
+//     $('.slideshow-container').each(function () {
+
+//         var highestBox = 0;
+//         $('.mySlides', this).each(function () {
+
+//             if ($(this).height() > highestBox)
+//                 highestBox = $(this).height();
+//         });
+
+//         $('.mySlides', this).height(highestBox);
+
+//     });
+// });
+
+
+const obj1 = document.getElementById('myInfo'); 
+const obj2 = document.getElementById('skills'); 
+
+const obj1_height = obj1.scrollHeight;
+const obj2_height = obj2.scrollHeight;
+
+if(obj1_height > obj2_height) { obj2.style.height = obj1_height + 'px'; }
+else { obj1.style.height = obj2_height + 'px'; }
