c9c88823dbc3e1f64b933d1c4f8808fa79584929
sh0402 <dgryoma0402@gmail.com>
2021-08-19 17:53:32 +0900

    0819_commit_corp

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 
-const toggleBtn = document.querySelector('.nav-button');
+// const toggleBtn = document.querySelector('.nav-button');
 
-toggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
+// toggleBtn.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+// });
 
 
-const cardCarouselNext = document.querySelector('.cardCarousel-control-next');
-const cardCarouselPrev = document.querySelector('.cardCarousel-control-prev');
-const cardCarousel = document.querySelector('.cardCarousel');
+// const cardCarouselNext = document.querySelector('.cardCarousel-control-next');
+// const cardCarouselPrev = document.querySelector('.cardCarousel-control-prev');
+// const cardCarousel = document.querySelector('.cardCarousel');
 
-cardCarouselPrev.addEventListener('click', () => {
-    cardCarousel.scrollLeft -= 100
-});
+// cardCarouselPrev.addEventListener('click', () => {
+//     cardCarousel.scrollLeft -= 100
+// });
 
-cardCarouselNext.addEventListener('click', () => {
-    cardCarousel.scrollLeft += 100
-});
+// cardCarouselNext.addEventListener('click', () => {
+//     cardCarousel.scrollLeft += 100
+// });
 
 
 // controlNextBtn = function() {
@@ -28,3 +28,10 @@ cardCarouselNext.addEventListener('click', () => {
 //     var _scrollX = $('.cardCarousel').scrollLeft();
 //     $('.cardCarousel').scrollLeft(_scrollX - 150);
 // };
+
+
+function cardCarousel (container) {
+    const slider = container.querySeletor('');
+}
+
+cardCarousel (document.querySelector('.cardCarousel'));
